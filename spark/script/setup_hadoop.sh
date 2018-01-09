c19cbe53b7d1b70e18e902333029bd8dd1af08df
niqdev <niqdev@gmail.com>
2018-01-09 16:58:32 +0000

    spark: draft setup scripts

---
+++ spark/script/setup_hadoop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# unofficial bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# run from any directory (no symlink allowed)
+CURRENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+cd ${CURRENT_PATH}
+
+echo "[+] setup hadoop"
+
+# TODO hadoop + yarn
+
+echo "[-] setup hadoop"
