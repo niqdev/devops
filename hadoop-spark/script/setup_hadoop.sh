ba84a1cd709c95bded04e5d26ff60047afca0d29
niqdev <niqdev@gmail.com>
2018-01-29 13:16:50 +0000

    hadoop-spark: draft refactor

--- hadoop-spark/script/setup_hadoop.sh
+++ hadoop-spark/script/setup_hadoop.sh
@@ -10,56 +10,77 @@ cd ${CURRENT_PATH}
 
 echo "[+] setup hadoop"
 
-GUEST_FILES_PATH="/vagrant/file"
+FILE_PATH="/vagrant/file"
+DATA_PATH="/vagrant/data"
 VAGRANT_HOME="/home/vagrant"
 
 HADOOP_VERSION="2.7.5"
-HADOOP_PATH="hadoop-$HADOOP_VERSION"
-HADOOP_DIST="$HADOOP_PATH.tar.gz"
+HADOOP_NAME="hadoop-$HADOOP_VERSION"
+HADOOP_DIST="$HADOOP_NAME.tar.gz"
+HADOOP_TMP_PATH="$DATA_PATH/$HADOOP_DIST"
 
-echo "[*] download dist"
-wget -q -P /tmp http://www-eu.apache.org/dist/hadoop/common/$HADOOP_PATH/$HADOOP_DIST
+function download_dist {
+  local HADOOP_MIRROR_DOWNLOAD="http://www-eu.apache.org/dist/hadoop/common/$HADOOP_NAME/$HADOOP_DIST"
 
-echo "[*] setup dist"
-tar -xzf /tmp/$HADOOP_DIST -C /opt
-rm /tmp/$HADOOP_DIST
-ln -s /opt/$HADOOP_PATH /usr/local/hadoop
+  echo "[*] download dist"
+  wget -q -P $DATA_PATH $HADOOP_MIRROR_DOWNLOAD
+}
 
-echo "[*] update env"
-echo -e "HADOOP_HOME=/usr/local/hadoop" | tee --append /etc/environment && \
-  source /etc/environment
-echo -e "export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin" | tee --append /etc/profile.d/hadoop.sh && \
-  source /etc/profile.d/hadoop.sh
+function setup_dist {
+  if [ ! -e $HADOOP_TMP_PATH ]; then
+    download_dist
+  fi
 
-echo "[*] create data directory"
-mkdir -p /var/hadoop/hadoop-datanode /var/hadoop/hadoop-namenode
+  echo "[*] setup dist"
+  tar -xzf $HADOOP_TMP_PATH -C /opt
+  #rm $HADOOP_TMP_PATH
+  ln -s /opt/$HADOOP_NAME /usr/local/hadoop
+}
 
-# http://hadoop.apache.org/docs/r2.7.5/hadoop-project-dist/hadoop-common/core-default.xml
-HOST_CONFIG_PATH="$HADOOP_HOME/etc/hadoop"
-FILES=( "core-site.xml" "hdfs-site.xml" )
-for FILE in "${FILES[@]}"
-do
-  echo "[*] update config: $FILE"
-  mv $HOST_CONFIG_PATH/$FILE $HOST_CONFIG_PATH/$FILE.orig
-  cp $GUEST_FILES_PATH/hadoop-$FILE $HOST_CONFIG_PATH/$FILE
-done
+function update_env {
+  echo "[*] update env"
+  echo -e "HADOOP_HOME=/usr/local/hadoop" | tee --append /etc/environment && \
+    source /etc/environment
+  echo -e "export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin" | tee --append /etc/profile.d/hadoop.sh && \
+    source /etc/profile.d/hadoop.sh
+}
 
-echo "[*] setup passphraseless ssh"
-ssh-keygen -t rsa -P '' -f $VAGRANT_HOME/.ssh/id_rsa
-cat $VAGRANT_HOME/.ssh/id_rsa.pub >> $VAGRANT_HOME/.ssh/authorized_keys
-chmod 0600 $VAGRANT_HOME/.ssh/authorized_keys
-cp $GUEST_FILES_PATH/ssh-config $VAGRANT_HOME/.ssh/config
+function setup_config {
+  echo "[*] create data directory"
+  mkdir -p /var/hadoop/hadoop-datanode /var/hadoop/hadoop-namenode
+  
+  local CONFIG_PATH="$HADOOP_HOME/etc/hadoop"
+  local FILES=( "core-site.xml" "hdfs-site.xml" )
+  for FILE in "${FILES[@]}"
+  do
+    echo "[*] update config: $FILE"
+    mv $CONFIG_PATH/$FILE $CONFIG_PATH/$FILE.orig
+    cp $FILE_PATH/hadoop-$FILE $CONFIG_PATH/$FILE
+  done
+}
+
+function setup_ssh {
+  echo "[*] setup ssh"
+  ssh-keygen -t rsa -P '' -f $VAGRANT_HOME/.ssh/id_rsa
+  cat $VAGRANT_HOME/.ssh/id_rsa.pub >> $VAGRANT_HOME/.ssh/authorized_keys
+  chmod 0600 $VAGRANT_HOME/.ssh/authorized_keys
+  cp $FILE_PATH/ssh-config $VAGRANT_HOME/.ssh/config
+}
 
 # TODO change user
-echo "[*] fix permissions"
-chown -R vagrant:vagrant /opt/$HADOOP_PATH $VAGRANT_HOME/.ssh /var/hadoop
+function update_permission {
+  echo "[*] update permission"
+  chown -R vagrant:vagrant /opt/$HADOOP_NAME $VAGRANT_HOME/.ssh /var/hadoop
+}
 
-echo "[*] init hdfs"
-su --login vagrant << EOF
-  source /etc/environment
-  source /etc/profile.d/hadoop.sh
-  hdfs namenode -format
-  hadoop version
+function init_hdfs {
+  echo "[*] init hdfs"
+  su --login vagrant << EOF
+    source /etc/environment
+    source /etc/profile.d/hadoop.sh
+    hdfs namenode -format
+    hadoop version
 EOF
+}
 
 echo "[-] setup hadoop"
