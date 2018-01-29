da18d39890a8ea007aed4087ed243621d4c6dce9
niqdev <niqdev@gmail.com>
2018-01-29 21:49:20 +0000

    hadoop-spark: draft hadoop user

--- hadoop-spark/script/setup_user.sh
+++ hadoop-spark/script/setup_user.sh
@@ -8,13 +8,39 @@ IFS=$'\n\t'
 CURRENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 cd ${CURRENT_PATH}
 
+##############################
+
+USER_NAME="hadoop"
+FILE_PATH="/vagrant/file"
+KEY_PATH="/vagrant/data/vagrant_rsa"
+
+##############################
+
 echo "[+] setup user"
 
-groupadd hadoop
-useradd --create-home --password hadoop --shell /bin/bash hadoop
-usermod --append --groups sudo,hadoop username
+function create_user {
+  local NAME=$1
+  useradd --create-home --shell /bin/bash $NAME
+  usermod --append --groups sudo,$NAME $NAME
+  echo "[*] create user: $NAME"
+  id $NAME
+  groups $NAME
+}
 
-id hadoop
-groups hadoop
+function config_ssh {
+  local SSH_PATH="/home/$USER_NAME/.ssh"
+  echo "[*] config ssh"
+  mkdir -p $SSH_PATH
+  cat $KEY_PATH.pub >> $SSH_PATH/authorized_keys
+  chmod 0600 $SSH_PATH/authorized_keys
+  cp $FILE_PATH/ssh-config $SSH_PATH/config
+}
+
+function main {
+  create_user $USER_NAME
+  config_ssh
+}
+
+main
 
 echo "[-] setup user"
