bc4806bed7e0f3e8b69006357b3d26d4ffd8175a
niqdev <niqdev@gmail.com>
2018-01-29 21:10:52 +0000

    hadoop-spark: update script

--- hadoop-spark/vagrant.sh
+++ hadoop-spark/vagrant.sh
@@ -11,29 +11,32 @@ cd ${CURRENT_PATH}
 ##############################
 
 BOX_NAME="default"
-KEY_PATH="data/vagrant_rsa"
+KEY_NAME="vagrant_rsa"
+KEY_PATH="data"
 
 ##############################
 
-function verify_requirements {
-  echo "[*] verify requirements"
-  command -v vagrant >/dev/null 2>&1 || (echo "[-] error: vagrant not found" && exit 1)
-  #command -v vboxmanage >/dev/null 2>&1 || (echo "[-] error: VirtualBox not found" && exit 1)
-  command -v ssh-keygen >/dev/null 2>&1 || (echo "[-] error: ssh-keygen not found" && exit 1)
+# param #1: <bin>
+function verify_requirement {
+  local BIN=$1
+  command -v $BIN >/dev/null 2>&1 || (echo "[-] error: $BIN not found" && exit 1)
 }
 
-# param: <path>
+# param #1: <name>
+# param #2: <path>
 function init_key_pair {
-  local PATH=$1
-  if [ ! -f $PATH ]; then
-    ssh-keygen -q -N "" -f $PATH
+  local NAME=$1
+  local PATH=$2/$NAME
+  if [ ! -e $PATH ]; then
+    #mkdir -p $PATH
+    #ssh-keygen -t rsa -b 4096 -C $NAME -N "" -f $PATH
     echo "[*] new ssh key pair generated: $PATH"
   else
     echo "[*] ssh key pair found: $PATH"
   fi
 }
 
-# param: <name>
+# param #1: <name>
 function start_vagrant {
   local NAME=$1
   local STATUS=$(vagrant status | grep $NAME | awk '{ print toupper($2) }')
@@ -53,9 +56,11 @@ function start_vagrant {
 }
 
 function main {
-  verify_requirements
-  init_key_pair
-  start_vagrant $BOX_NAME
+  verify_requirement vagrant
+  verify_requirement ssh-keygen
+
+  init_key_pair $KEY_NAME $KEY_PATH
+  #start_vagrant $BOX_NAME
 }
 
 main
