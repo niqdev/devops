ea74bf3bb22db8ae9f7c427d71883dd96038641b
niqdev <niqdev@gmail.com>
2018-01-29 21:31:06 +0000

    hadoop-spark: update vagrant script

--- hadoop-spark/vagrant.sh
+++ hadoop-spark/vagrant.sh
@@ -12,7 +12,7 @@ cd ${CURRENT_PATH}
 
 BOX_NAME="default"
 KEY_NAME="vagrant_rsa"
-KEY_PATH="data"
+DATA_PATH="data"
 
 ##############################
 
@@ -26,26 +26,30 @@ function verify_requirement {
 # param #2: <path>
 function init_key_pair {
   local NAME=$1
-  local PATH=$2/$NAME
-  if [ ! -e $PATH ]; then
-    #mkdir -p $PATH
-    #ssh-keygen -t rsa -b 4096 -C $NAME -N "" -f $PATH
-    echo "[*] new ssh key pair generated: $PATH"
+  local BASE_PATH=$2
+  local KEY_PATH="$BASE_PATH/$NAME"
+
+  if [ ! -e $KEY_PATH ]; then
+    mkdir -p $BASE_PATH
+    ssh-keygen -t rsa -b 4096 -C $NAME -N "" -f $KEY_PATH
+    echo "[*] new ssh key pair generated: $KEY_PATH"
   else
-    echo "[*] ssh key pair found: $PATH"
+    echo "[*] ssh key pair found: $KEY_PATH"
   fi
 }
 
 # param #1: <name>
 function start_vagrant {
   local NAME=$1
-  local STATUS=$(vagrant status | grep $NAME | awk '{ print toupper($2) }')
+  local STATUS=$(vagrant status | grep -m 1 $NAME | awk '{ print toupper($2) }')
   echo -e "[*] name=$NAME | status=$STATUS"
 
   case $STATUS in
-    "POWEROFF")
+    # not created | poweroff
+    "NOT"|"POWEROFF")
       vagrant up && vagrant ssh
       ;;
+    # running
     "RUNNING")
       vagrant ssh
       ;;
@@ -59,8 +63,8 @@ function main {
   verify_requirement vagrant
   verify_requirement ssh-keygen
 
-  init_key_pair $KEY_NAME $KEY_PATH
-  #start_vagrant $BOX_NAME
+  init_key_pair $KEY_NAME $DATA_PATH
+  start_vagrant $BOX_NAME
 }
 
 main
