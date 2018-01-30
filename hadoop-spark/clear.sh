09f344bfa9b93a98f858ef66a3c07d4027aa29c6
niqdev <niqdev@gmail.com>
2018-01-30 19:01:56 +0000

    hadoop-spark: add remove script

---
+++ hadoop-spark/clear.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# unofficial bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# run from any directory (no symlink allowed)
+CURRENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+cd ${CURRENT_PATH}
+
+##############################
+
+explosion() {
+cat<<"EOT"
+                             ____
+                     __,-~~/~    `---.
+                   _/_,---(      ,    )
+               __ /        <    /   )  \___
+- ------===;;;'====------------------===;;;===----- -  -
+                  \/  ~"~"~"~"~"~\~"~)~"/
+                  (_ (   \  (     >    \)
+                   \_( _ <         >_>'
+                      ~ `-i' ::>|--"
+                          I;|.|.|
+                         <|i::|i|`.
+                        (` ^'"`-' ")
+------------------------------------------------------------------
+
+EOT
+}
+
+read -p "Are you sure? [y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  vagrant destroy -f
+  rm -fr .vagrant data/hadoop_rsa*
+  explosion
+fi
